2d53f31e30eed41e521d5b537c73708f5df59bfa
jiangliu123456 <119939510+jiangliu123456@users.noreply.github.com>
2024-05-31 00:08:08 +0800

    Update pthread_pool.c

--- pthread_pool.c
+++ pthread_pool.c
@@ -9,7 +9,7 @@
 #include "pthread_pool.h"
 
 #define _CAS_(ptr, old, new)    __sync_bool_compare_and_swap(ptr, old, new)
-#define FREE_LOCK(lock)         while(!_CAS_(&(lock),0,1)) nanosleep(0,0)
+#define FREE_LOCK(lock)         while(!_CAS_(&(lock),0,1)) sleep(0)
 #define FREE_UNLOCK(lock)       lock = 0
 
 typedef struct {
@@ -41,6 +41,11 @@ volatile int efd_r_lock  = 0;//efd_queue 读端 锁
 volatile int efd_w_lock  = 0;//efd_queue 写端 锁
 volatile int efd_cnt     = 0;//efd_queue 成员个数
 
+volatile int cnt_idx     = 0;
+volatile int cnt_arry[8];
+volatile int sum = 0;
+volatile int timerout_max = 0;
+
 /*定时器函数,精度us*/
 int set_timer_us(int s,int us)
 {
@@ -69,13 +74,9 @@ void *worker(void *arg) {
     int timerout_cnt = 0;
     int id = *((int*)arg);
 
-    int timerout_max = 0;
-    int cnt_arry[8];
-    int sum = 0;
     int water_line = (QUEUE_SIZE/THREAD_COUNT)/16;
-    int cnt_idx = 0;
     printf("worker %d start\n", id);
-    
+    memset(cnt_arry, 0, sizeof(cnt_arry));
     while (g_runing_stat == E_RUNNING_STAT_RUN) {
         /*count=0;阻塞*/
         re:
@@ -84,21 +85,20 @@ void *worker(void *arg) {
             if (timerout_cnt >= timerout_max) {
                 //printf("-------timerout_max = %d\n", timerout_max);
                 FREE_LOCK(efd_w_lock);
-                efd_queue[efd_w] = evefd[id];
-                efd_w = (efd_w+1)%THREAD_COUNT;
-                __sync_fetch_and_add(&efd_cnt, 1);
+                __sync_fetch_and_and(&efd_cnt, 1<<id);
                 FREE_UNLOCK(efd_w_lock);
-
-                //printf("pthread_cond_wait %d\n", id);
-                int _ = read(evefd[id], &val, sizeof(val));
-                //printf("pthread_cond_wait %d over\n", id);
+				int v;
+                //printf("pthread_cond_wait %d fd=%d\n", id, evefd[id]);
+                int _ = read(evefd[id], &v, sizeof(v));
+                //printf("pthread_cond_wait %d over fd=%d v=%d\n", id, evefd[id],v);
                 timerout_cnt = 0;
             } else {
                 int _ = read(timerfd, &val, sizeof(val));//sleep 1us
                 timerout_cnt++;
             }
         }
-        
+		
+        FREE_LOCK(r_lock);
         //timerout_max 自适应算法
         if ((++cnt_idx & 7) ==0) {
             int h3 = cnt_idx>>3;
@@ -106,13 +106,14 @@ void *worker(void *arg) {
                 cnt_idx = 0;
                 h3 = 0;
             }
-            sum = sum + count - cnt_arry[h3];
+			//sum = sum +count - cnt_arry[h3];
             cnt_arry[h3] = count;
-            timerout_max = (sum>>3)>water_line?10:0;
+			int sum2 = 0;
+			for(int j=0;j<8;j++)
+				sum2+=cnt_arry[j];
+            timerout_max = ((sum2>>3)>10)?10:1;
         }
-        timerout_cnt=0;
         
-        FREE_LOCK(r_lock);
         if (count==0) {
             FREE_UNLOCK(r_lock);
             goto re;
@@ -125,6 +126,7 @@ void *worker(void *arg) {
         FREE_UNLOCK(r_lock);
 
         (task.function)(task.data);
+		timerout_cnt=0;
     }
     return NULL;
 }
@@ -138,7 +140,7 @@ void thread_pool_init() {
     
     int i = 0;
     for (i=0;i<THREAD_COUNT;i++) {
-        evefd[i] = eventfd(0, /*EFD_NONBLOCK | */EFD_CLOEXEC);
+        evefd[i] = eventfd(0, /*EFD_NONBLOCK | */EFD_CLOEXEC|EFD_SEMAPHORE);
         if (evefd[i] == -1) {
             goto err_evefd;
         }
@@ -185,17 +187,27 @@ void enqueue_task(void (*function)(void *), void *data) {
     queue_rear = (queue_rear + 1) % QUEUE_SIZE;
     __sync_fetch_and_add(&count, 1);
     FREE_UNLOCK(w_lock);
-
+	
+re_notify:
     if (!_CAS_(&efd_cnt,0,0)) {
         
-        FREE_LOCK(efd_r_lock);
-        int fd = efd_queue[efd_r];
-        efd_r = (efd_r+1)%THREAD_COUNT;
-        __sync_fetch_and_sub(&efd_cnt, 1);
-        FREE_UNLOCK(efd_r_lock);
+        FREE_LOCK(efd_w_lock);
+		int k  = 0;
+	    int temp = efd_cnt;
+		while (temp) {
+			if (temp&1) {
+				break;
+			}
+			temp>>=1;
+			k++;
+		}
+        int fd = evefd[k];
 
-        ssize_t _ = write(fd, &fd, 4);
+        if (-1 == write(fd, &fd, 4)) {
+			FREE_UNLOCK(efd_w_lock);
+			goto re_notify;
+		}
+        __sync_fetch_and_or(&efd_cnt, ~(1<<k));
+        FREE_UNLOCK(efd_w_lock);
     }
-    
-    
 }
